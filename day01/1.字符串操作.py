f4d0f73f75094c69fa45e4b7d1891c0925bc2141
lc2019 <18192948295@189.cn>
2020-03-28 10:23:50 +0800

    Initial commit

---
+++ day01/1.字符串操作.py
@@ -0,0 +1,13 @@
+#sep 分割符" "
+# end 结束以后的默认输出\n
+# 一个或多个值使用,来分割
+# ,表示当前的sep符
+# \n 换行符
+
+
+# print(value, ..., sep=' ', end='\n', file=sys.stdout, flush=False)
+print('hello', 'boy' '2020' 'goon')
+print('hello', 'boy' '2020' 'goon',sep='+')
+print('hello', 'boy' '2020' 'goon',sep='+',end='==='"\n")
+print("haha")
+print()
