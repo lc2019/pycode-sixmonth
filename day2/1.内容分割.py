f80cc23815734d860f651fced9d339df34cdabb1
lc2019 <18192948295@189.cn>
2020-03-29 13:17:05 +0800

    3/29

--- day2/1.内容分割.py
+++ day2/1.内容分割.py
@@ -1,2 +1,17 @@
 # split splitlines parttion rparttion
 s = 'zhangsan,lisi,wangwu,zhaoliu'
+s1 = s.split(',')  # 切割以后的结果就是列表
+print(s1)
+s2 = s.split(',', 2)  # 分割次数
+print(s2)
+
+print('asdcasd'.partition('c'))  # ('asd', 'c', 'asd')
+print('asdcasd'.rpartition('d'))  # ('asdcas', 'd', '')
+
+# 获取文件名和后缀名 x.mp3
+filename = 'x.mp4'
+t = filename.partition('.')
+print(t)
+
+filename = '2020.03.28-x.mp4'
+print(filename.rpartition('.'))
---
+++ day2/2.字符编码.py
@@ -0,0 +1,4 @@
+# chr ord
+print(ord('a')) #97
+print(chr(65)) #A 数字对应的编码
+
