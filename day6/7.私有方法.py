be897b9b278f58838ca4d19d7c43476478a663f7
lc2019 <18192948295@189.cn>
2022-11-27 23:03:48 +0800

    1127

--- day6/7.私有方法.py
+++ day6/7.私有方法.py
@@ -1,8 +1,16 @@
 class Thd(object):
     def __init__(self):
-        self.__list=[]
+        self.__list = []
 
-    def add_task(self,url):
+    def add_task(self, url):
         self.__list.append(url)
+        # 通过公有的方法调用私有方法
+        self.__download_date(url)
 
-    def
+    # 私有方法
+    def __download_date(self, url):
+        print(f'url is downing ...{url}')
+
+
+th = Thd()
+th.add_task('http://baidu.com/a.mp4')
