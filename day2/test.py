53b85c48190ebbe3081b420acbfb3c9ae371c9b6
lc2019 <18192948295@189.cn>
2022-11-27 22:52:04 +0800

    1127

--- day2/test.py
+++
@@ -1,7 +0,0 @@
-num1 = 1
-num2 = 1
-for i in range(0, 10):
-    a = num1
-    num1 = num2
-    num2 = a + num2
-    print(num1, num2)
--- day3/8.多个返回值.py
+++
@@ -1,8 +0,0 @@
-def test(a, b):
-    x = a // b
-    y = a % b
-    return x, y
-
-
-res = test(13, 5)
-print(res[0], res[1])
